a0a4121117613075500d7272497bdbe079c61400
Max Marlatt <m.marlatt@setonhill.edu>
2020-03-10 11:33:47 -0400

    Initial Commit

---
+++ Navigation IN class/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation IN class
+//
+//  Created by Maxwell Marlatt on 3/10/20.
+//  Copyright © 2020 Maxwell Marlatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Navigation IN classTests/Navigation_IN_classTests.swift
@@ -0,0 +1,34 @@
+//
+//  Navigation_IN_classTests.swift
+//  Navigation IN classTests
+//
+//  Created by Maxwell Marlatt on 3/10/20.
+//  Copyright © 2020 Maxwell Marlatt. All rights reserved.
+//
+
+import XCTest
+@testable import Navigation_IN_class
+
+class Navigation_IN_classTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
